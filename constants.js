5c7558ea77e5aefa63e9e8e2e5be1969c9af1c65
SAIMALK <aleesasaim496@gmail.com>
2024-08-15 18:54:41 +0500

    aa

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://192.168.1.9:5000';
-export const UPLOAD_URL = 'http://192.168.1.9:5000/api/upload'
-export const USERS_URL = 'http://192.168.1.9:5000/api/users';
-export const ADDRESS = 'http://192.168.1.9:5000';
-export const CROPS_URL = 'http://192.168.1.9:5000/api/crops';
+export const BASE_URL = 'http://192.168.1.8:5000';
+export const UPLOAD_URL = 'http://192.168.1.8:5000/api/upload'
+export const USERS_URL = 'http://192.168.1.8:5000/api/users';
+export const ADDRESS = 'http://192.168.1.8:5000';
+export const CROPS_URL = 'http://192.168.1.8:5000/api/crops';
